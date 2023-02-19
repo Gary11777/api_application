1b3e0adbf0d7dc3dd43fbe3d8014465fedcff051
Gary11777 <iharushakou@gmail.com>
2023-02-19 19:13:15 +0300

    reset_password2 19.02.2023 19:12

---
+++ tests/Feature/ResetPasswordTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\ResetPassword;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class ResetPasswordTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function test_resetPassword_method()
+    {
+        $this->artisan('passport:install');
+        $pass = 'user1';
+        $password_hash = Hash::make($pass);
+        $user = User::factory()->create([
+            'email' => 'user1@gmail.com',
+            'password' => $password_hash
+        ]);
+        $response = $this->json('POST', 'api/reset_password',[
+            'email' => $user->email
+        ])->assertStatus(200);
+        $response->assertJsonStructure(['result', 'message']);
+    }
+
+    public function test_setNewPassword_method()
+    {
+        $this->artisan('passport:install');
+        $pass = 'user1';
+        $password_hash = Hash::make($pass);
+        $user = User::factory()->create([
+            'email' => 'user1@gmail.com',
+            'password' => $password_hash
+        ]);
+        $token = Str::random(10);
+        DB::table('reset_passwords')->insert([
+            'user_id' => 1,
+            'token' => $token
+        ]);
+        $response = $this->json('POST', 'api/set_newpassword',[
+            'token' => $token,
+            'password' => '123'
+        ])->assertStatus(200);
+        $response->assertJsonStructure(['result', 'message']);
+    }
+}
---
+++ tests/Unit/ResetPasswordTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use App\Services\UserService;
+use PHPUnit\Framework\TestCase;
+
+class ResetPasswordTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_resetPassword_method()
+    {
+        $this->assertTrue(true);
+    }
+}
