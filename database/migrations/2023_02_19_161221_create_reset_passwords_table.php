6caa8b7113abdab5375b4f077d414efe2dc2b26d
Gary11777 <iharushakou@gmail.com>
2023-02-19 19:19:08 +0300

    reset_password2 19.02.2023 19:19

--- database/migrations/2023_02_19_161221_create_reset_passwords_table.php
+++ database/migrations/2023_02_19_161221_create_reset_passwords_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('reset_passwords', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('user_id');
+            $table->string('token');
             $table->timestamps();
         });
     }
